17d373d781ec33a130c03bacaa81c0ccb9b3467b
Yarden Zeron <zeron.yarden@gmail.com>
2023-01-18 11:32:24 +0200

    root combine root

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 import routes from '../routes'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { login, logout, signup } from '../store/user.actions.js'
@@ -37,38 +37,47 @@ export function AppHeader() {
         <header className="app-header main-container">
             <main className='main-header'>
 
-            <div className="logo">
-                AnyWhere
-            </div>
-
-            <nav>
-                <NavLink key={"anywhere"}>
-                    <p>anywhere</p>
-                </NavLink>
-                <NavLink key={"anyWeek"}>
-                    <p>any week</p>
-                </NavLink>
-                <NavLink key={"addguests"}>
-                    <p>addguests</p>
-                </NavLink>
-
-                <div className="serachIcon">
-                    <img src="../assets/img/icons/search.png" alt="serach" />
+                <div className="logo">
+                    AnyWhere
                 </div>
-            </nav>
 
-            <div className="users">
-                <div className="switchToHost">
-                    <p>switch to hosting</p>
-                </div>
-                <div className="languageOpstion">
-                <span className="material-symbols-outlined">
-                         language
+                <nav>
+                    <NavLink key={"anywhere"}>
+                        <p>anywhere</p>
+                    </NavLink>
+                    <NavLink key={"anyWeek"}>
+                        <p>any week</p>
+                    </NavLink>
+                    <NavLink key={"addguests"}>
+                        <p>addguests</p>
+                    </NavLink>
+
+                    <div className="serachIcon">
+                        <img src="../assets/img/icons/search.png" alt="serach" />
+                    </div>
+                </nav>
+
+                <div className="usersOpstion">
+                    <div className="switchToHost">
+                        <p>switch to hosting</p>
+                    </div>
+                    <div className="languageOpstion">
+                        <span className="material-symbols-outlined">
+                            language
                         </span>
-                </div>
-            </div>
+                    </div>
+                    <div className="loginMenu">
+                        <span class="material-symbols-outlined">
+                            menu
+                        </span>
+                        <span class="material-symbols-outlined">
+                            person
+                        </span>
 
-            {/* <nav>
+                    </div>
+                </div>
+
+                {/* <nav>
                 {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
 
                 {user &&
