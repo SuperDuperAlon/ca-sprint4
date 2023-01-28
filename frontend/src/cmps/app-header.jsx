dfcfbc7b16ee5e6a8c2d609dfd3a98e5c9625d60
Yarden Zeron <zeron.yarden@gmail.com>
2023-01-28 18:19:03 +0200

    app header

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -14,7 +14,7 @@ import { SecondaryFilter } from '../pages/secondary-filter'
 import { useState } from 'react'
 import { filterService } from '../services/filterService'
 
-export function AppHeader({onToSearch, stay}) {
+export function AppHeader({onToSearch, stay ,onClickOutSideTheBar}) {
     const openSearchBar = useSelector(storeState => storeState.stayModule.searchModalOpen)
     const navigate = useNavigate()
 
@@ -30,7 +30,7 @@ export function AppHeader({onToSearch, stay}) {
                 <div className="logo" onClick={() => navigate('/')}>
                     <div><span className="a-Logo"><SiAirbnb /></span><span className='text-next-to-logo'>nyplace</span></div>
                 </div>
-                <div className="center-grid">
+                <div className={`center-grid ${!openSearchBar? "" : "close"}`}>
                     <div className={!openSearchBar ? 'rounded-full nav-bar-header' : " rounded-full nav-bar-header close"}>
                         <nav className='header-nav long'>
                             <NavLink key={"anywhere"} onClick={() => onChangeStaySearchBar('location')}>
@@ -72,7 +72,7 @@ export function AppHeader({onToSearch, stay}) {
                     </div>
                 </div>
                 <div className="users-option fs-14">
-                    <div className="header-options-switching-keys mar-r8">
+                    <div className="header-options-switching-keys">
                         <div className="switch-to-host">
                             <div className='fs14 lh18 pad-all12 rounded-full bold switch-to-host'>
                                 switch to hosting</div>
@@ -95,8 +95,8 @@ export function AppHeader({onToSearch, stay}) {
             <div className='cover-filter full'>
                 <div className={openSearchBar? "open": ''}></div>
             </div>
-           { !openSearchBar && <div className="line full"></div>}
-            <SearchBar onToSearch={onToSearch} />
+           {!openSearchBar && <div className="line full"></div>}
+            <SearchBar onToSearch={onToSearch} onClickOutSideTheBar={onClickOutSideTheBar} />
             {!stay && <SecondaryFilter onToSearch={onToSearch} />}
 
         </>
