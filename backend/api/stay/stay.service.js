571b6dcffff59cae9bfe52ce2af687b511aa2d4c
Alon Mlievski <alonmlievski@gmail.com>
2023-01-29 22:35:27 +0200

    remove stay data from code

--- backend/api/stay/stay.service.js
+++ backend/api/stay/stay.service.js
@@ -2,7 +2,6 @@ const dbService = require("../../services/db.service");
 const logger = require("../../services/logger.service");
 const utilService = require("../../services/util.service");
 const ObjectId = require("mongodb").ObjectId;
-const stays = require("../../data/stay.json");
 const { log } = require("../../middlewares/logger.middleware");
 const fs = require("fs");
 
