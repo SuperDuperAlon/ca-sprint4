5986c8369e42d3c55226f23c5693f6bf5443c8eb
Alon Mlievski <alonmlievski@gmail.com>
2023-01-29 23:21:31 +0200

    test

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,10 +1,5 @@
 module.exports = {
-  dbURL: 'mongodb+srv://superduperalon:UB6vekjsMRz8fkfV@cluster0.711rchj.mongodb.net/?retryWrites=true&w=majority',
-  dbName : 'stay_db'
+  dbURL:
+    'mongodb+srv://superduperalon:UB6vekjsMRz8fkfV@cluster0.711rchj.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'stay_db',
 }
-
-
-// module.exports = {
-//   dbURL: 'mongodb+srv://superduperalon:UB6vekjsMRz8fkfV@cluster0.711rchj.mongodb.net/?retryWrites=true&w=majority',
-//   dbName : 'stay_db'
-// }
