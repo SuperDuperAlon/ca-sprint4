358a58df214d0b7859b9ec12b6731d0725372194
Alon Mlievski <alonmlievski@gmail.com>
2023-01-29 22:39:53 +0200

    setting to production

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -2,3 +2,9 @@ module.exports = {
   dbURL: 'mongodb+srv://superduperalon:UB6vekjsMRz8fkfV@cluster0.711rchj.mongodb.net/?retryWrites=true&w=majority',
   dbName : 'stay_db'
 }
+
+
+// module.exports = {
+//   dbURL: 'mongodb+srv://superduperalon:UB6vekjsMRz8fkfV@cluster0.711rchj.mongodb.net/?retryWrites=true&w=majority',
+//   dbName : 'stay_db'
+// }
--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,15 @@ app.use(express.json())
 // app.use(express.static('public'))
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
     const corsOptions = {
         origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
     }
     app.use(cors(corsOptions))
-// }
+}
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
