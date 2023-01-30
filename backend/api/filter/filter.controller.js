f7cd8d818b26f50230cf5b58e81771eb452eec6f
Yarden Zeron <zeron.yarden@gmail.com>
2023-01-30 16:15:37 +0200

    search bar by text

---
+++ backend/api/filter/filter.controller.js
@@ -0,0 +1,22 @@
+const filterService = require("./filter.service.js");
+
+const logger = require("../../services/logger.service");
+
+async function getStaysByText(req, res) {
+  try {
+    logger.debug("searchByText");
+    text= req.query?.text || ""
+    
+    const data = await filterService.query(text);
+    console.log('data:', data)
+    res.json(data);
+  } catch (err) {
+    logger.error("Failed to get stays", err);
+    res.status(500).send({ err: "Failed to get stays" });
+  }
+}
+
+module.exports = {
+  getStaysByText,
+  
+};
