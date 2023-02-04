acda40dafebef64a7f2fb773bda3e614d23c419f
Alon Mlievski <alonmlievski@gmail.com>
2023-02-04 18:13:58 +0200

    remove config files from gitignore

---
+++ backend/config/prod.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   dbURL: 'mongodb+srv://noahmarko93:Qtnc5obt4cNzrkff@cluster0.s1su5ry.mongodb.net/?retryWrites=true&w=majority',
+//   dbName: 'mystay_db',
+// }
+
+module.exports = {
+  dbURL:
+    'mongodb+srv://superduperalon:8j2bA7ckY8w5TiEF@cluster0.s1su5ry.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'mystay_db',
+}
