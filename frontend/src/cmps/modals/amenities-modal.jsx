1c6e3063e7cd2d11c5c4665588f5d055e7ad980f
Alon Mlievski <alonmlievski@gmail.com>
2023-03-12 13:09:55 +0200

    amenities modal pre-design

--- frontend/src/cmps/modals/amenities-modal.jsx
+++ frontend/src/cmps/modals/amenities-modal.jsx
@@ -1,20 +1,21 @@
-export function AmenitiesModal({closeAmenitiesModal, stay}) {
-
+export function AmenitiesModal({ closeAmenitiesModal, stay }) {
   function BlockModal(ev) {
-    ev.stopPropagation()
-    return null
+    ev.stopPropagation();
+    return null;
   }
 
-  if (!stay) return console.log('wait');
-else return (
-    <>
-      <div className="black-screen full" onClick={closeAmenitiesModal}>
-        <div className="modal" onClick={(ev) => BlockModal(ev)}>
-  {        console.log(stay)}
-  
-          <h1>amenities Modal</h1>
+  if (!stay) return console.log("wait");
+  else
+    return (
+      <>
+        <div className="black-screen full" onClick={closeAmenitiesModal}>
+          <div className="modal" onClick={(ev) => BlockModal(ev)}>
+      <h3>What this place offers</h3>
+            {stay.amenities.map((item) => (
+              <li>{item}</li>
+            ))}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 }
