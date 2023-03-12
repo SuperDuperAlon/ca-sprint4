895ab0f13dfc068be08f1d7391ced06fc19108c2
Alon Mlievski <alonmlievski@gmail.com>
2023-03-12 14:29:20 +0200

    review modal setting in details page

--- frontend/src/cmps/stay-details/stay-details-locations-info.jsx
+++ frontend/src/cmps/stay-details/stay-details-locations-info.jsx
@@ -1,49 +1,65 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
 import { FiShare, FiHeart } from "react-icons/fi";
 import { utilService } from "../../services/util.service";
+import { ReviewModal } from "../modals/review-modal";
 
 export function StayDetailsLocationInfo({ stay, isMobile }) {
+  // Reviews Modal
+  const [reviewModal, setReviewModal] = useState(false);
+  const openReviewModal = () => setReviewModal(true);
+  const closeReviewModal = () => setReviewModal(false);
+
   if (!stay) return console.log("lala");
   else
     return (
-      <div className="details-locations-info">
-        <div className="info-header fs26 fw600 lh30">{stay.name}</div>
-        <div className="secondary-info fs14 flex align-center">
-          <div className="locations-attr fs14 fw600">
-            <div className="mar-r4">
-              <span className={isMobile ? "fs14" : "fs16"}>
-                <AiFillStar />
-              </span>
-              {stay.reveiwRate
-                ? +stay.reveiwRate
-                : +utilService.getRandomFloatInclusive(4, 5).toFixed(2)}{" "}
-              · <Link to={`/reviews/`}>{stay.reviews.length} reviews</Link>
+      <>
+        <div className="details-locations-info">
+          <div className="info-header fs26 fw600 lh30">{stay.name}</div>
+          <div className="secondary-info fs14 flex align-center">
+            <div className="locations-attr fs14 fw600">
+              <div className="mar-r4">
+                <span className={isMobile ? "fs14" : "fs16"}>
+                  <AiFillStar />
+                </span>
+                {stay.reveiwRate
+                  ? +stay.reveiwRate
+                  : +utilService.getRandomFloatInclusive(4, 5).toFixed(2)}{" "}
+                ·{" "}
+                <button onClick={openReviewModal}>
+                  {stay.reviews.length} reviews
+                </button>
+              </div>
+              ·
+              {stay.host.isSuperhost && (
+                <div className="fw400">Superhost ·</div>
+              )}
+              <div className="mar-l4">
+                <button>{stay.loc.address}</button>
+              </div>
             </div>
-            ·{stay.host.isSuperhost && <div className="fw400">Superhost ·</div>}
-            <div className="mar-l4">
-              <Link to={`/location/`}>{stay.loc.address}</Link>
-            </div>
+            {isMobile ? (
+              ""
+            ) : (
+              <div className="details-locations-info-actions fs14 lh18 ">
+                <button className="save-share fw600">
+                  <div>
+                    <FiShare />
+                  </div>
+                  <div className="txt-d-ul mar-l8">Share</div>{" "}
+                </button>
+                <button className="save-share fw600">
+                  <div>
+                    <FiHeart />
+                  </div>
+                  <div className="txt-d-ul mar-l8">Save</div>
+                </button>
+              </div>
+            )}
           </div>
-          {isMobile ? (
-            ""
-          ) : (
-            <div className="details-locations-info-actions fs14 lh18 ">
-              <button className="save-share fw600">
-                <div>
-                  <FiShare />
-                </div>{" "}
-                <div className="txt-d-ul mar-l8">Share</div>{" "}
-              </button>
-              <button className="save-share fw600">
-                <div>
-                  <FiHeart />
-                </div>
-                <div className="txt-d-ul mar-l8">Save</div>
-              </button>
-            </div>
-          )}
         </div>
-      </div>
+        {reviewModal ? <ReviewModal closeReviewModal={closeReviewModal} stay={stay}/> : ''}
+      </>
     );
 }
--- frontend/src/cmps/stay-details/stay-details-reservation-modal.jsx
+++ frontend/src/cmps/stay-details/stay-details-reservation-modal.jsx
@@ -1,144 +1,219 @@
-import { useState } from "react"
-import { useSelector } from "react-redux"
-import { Link, useNavigate, useParams } from "react-router-dom"
-import "react-dates/initialize"
-import { DateRangePicker } from "react-dates"
-import "react-dates/lib/css/_datepicker.css"
-import { OrderPreferences } from "../order-preferences"
-import { orderService } from "../../services/order.service"
-import { addOrder } from "../../store/order.actions"
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { CalendarMain } from "../filter/calendar"
-import { filterService } from "../../services/filterService"
-import {MdStar} from "react-icons/md"
-import {BiChevronDown, BiChevronUp} from "react-icons/bi"
-import { GuestsCounter } from "../filter/guests-counter"
-import { utilService } from "../../services/util.service"
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import "react-dates/initialize";
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import { OrderPreferences } from "../order-preferences";
+import { orderService } from "../../services/order.service";
+import { addOrder } from "../../store/order.actions";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { CalendarMain } from "../filter/calendar";
+import { filterService } from "../../services/filterService";
+import { MdStar } from "react-icons/md";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+import { GuestsCounter } from "../filter/guests-counter";
+import { utilService } from "../../services/util.service";
+import { ReviewModal } from "../modals/review-modal";
 
 export function StayDetailsOrderModal({ stay }) {
-  const [startDate, setStartDate] = useState(null)
-  const [endDate, setEndDate] = useState(null)
-  const [focusedInput, setFocusedInput] = useState()
-  const [isDateClicked, setIsDateClicked] = useState(false)
-  const [isGuestsClicked, setIsGuestsClicked] = useState(false)
-  const { stayId , filter } = useParams()
-  const [filterBy, setFilterBy] = useState(filterService.getParamsToObj(filter))
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [focusedInput, setFocusedInput] = useState();
+  const [isDateClicked, setIsDateClicked] = useState(false);
+  const [isGuestsClicked, setIsGuestsClicked] = useState(false);
+  const { stayId, filter } = useParams();
+  const [filterBy, setFilterBy] = useState(
+    filterService.getParamsToObj(filter)
+  );
   // const [daysCount, setDaysCount] = useState(null)
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  //Review Modal
+  const [reviewModal, setReviewModal] = useState(false);
+  const openReviewModal = () => setReviewModal(true);
+  const closeReviewModal = () => setReviewModal(false);
 
-
-const serviceFee = 55
+  const serviceFee = 55;
 
   async function onReserve() {
+    const queryParams = `checkIn=${filterBy.checkIn}&checkOut=${filterBy.checkOut}&adults=${filterBy.adults}&children=${filterBy.children}&infants=${filterBy.infants}&pets=${filterBy.pets}`;
 
-
-    const queryParams = 
-        `checkIn=${filterBy.checkIn}&checkOut=${filterBy.checkOut}&adults=${filterBy.adults}&children=${filterBy.children}&infants=${filterBy.infants}&pets=${filterBy.pets}`
-
-    navigate(`/book/stay/${stay._id}/${queryParams}`)
+    navigate(`/book/stay/${stay._id}/${queryParams}`);
   }
 
   // const numOfDays = calculateDays()
 
-  function calculateDays(){
-    const days = (new Date(filterBy.checkOut)-new Date(filterBy.checkIn))/(1000 * 60 * 60 * 24)
+  function calculateDays() {
+    const days =
+      (new Date(filterBy.checkOut) - new Date(filterBy.checkIn)) /
+      (1000 * 60 * 60 * 24);
     // setDaysCount(days)
-    return days>1 ? days+ ' nights' : days + ' night'
+    return days > 1 ? days + " nights" : days + " night";
   }
 
-  function onChangeDate(dates){
-        const checkIn = dates[0]
-        const checkOut = dates[1]
-        // calculateDays()
-        if(dates[1]){
-            toggleDatePicker()
-        }
-        setFilterBy({ ...filterBy, checkOut, checkIn })  
+  function onChangeDate(dates) {
+    const checkIn = dates[0];
+    const checkOut = dates[1];
+    // calculateDays()
+    if (dates[1]) {
+      toggleDatePicker();
+    }
+    setFilterBy({ ...filterBy, checkOut, checkIn });
   }
 
-  function toggleDatePicker(){
-      isDateClicked ? setIsDateClicked(false): setIsDateClicked(true)
+  function toggleDatePicker() {
+    isDateClicked ? setIsDateClicked(false) : setIsDateClicked(true);
   }
   // guests, ...filterBy.guests[field] = filterBy.guests[field]
   function onCountChange(field, diff) {
     // const prevGuests = { ...filterBy, [field] : filterBy[field]  + diff }
     // console.log(prevGuests);
-    setFilterBy({...filterBy, [field] : filterBy[field]  + diff})
-}
+    setFilterBy({ ...filterBy, [field]: filterBy[field] + diff });
+  }
 
-  if (!stay) return 
-  else return (
+  if (!stay) return;
+  else
+    return (
       <section className="order-form-container">
-      <section className="order-form">
-        <div className="order-info mar-b24">
-          <div className="fs22 fw600">
-            ${stay.price} <span className="fs16">night</span>{" "}
+        <section className="order-form">
+          <div className="order-info mar-b24">
+            <div className="fs22 fw600">
+              ${stay.price} <span className="fs16">night</span>{" "}
+            </div>
+            <div className="fs14 fw600">
+              <MdStar />
+              4.88 ·
+              <button onClick={openReviewModal} className="sml-review-btn">
+                {stay.reviews.length} reviews
+              </button>
+            </div>
           </div>
-          <div className="fs14 fw600">
-          <MdStar />4.88 ·<button className="sml-review-btn"><Link to={`/review/`}>{stay.reviews.length} reviews</Link> </button>{" "}
-          </div>
-        </div>
-        <div className="set-dates">
-          <div className="order-details-setting mar-b16">
-            <button className="order-form-btn up" onClick={toggleDatePicker}>
-              <div className="date-container flex column">
-                <div className="upp-left-14-600 bold fs9">CHECK-IN</div>
-                  <div className="upp-left-14-600 mar-r8">{filterBy?.checkIn ? new Date(filterBy.checkIn).toLocaleDateString(): <span className="grey-71">Add date</span>}</div>
-              </div>
-              <div className="date-container flex column">
-                <div className="upp-left-14-600 bold fs9">CHECKOUT</div>
-                  <div className="upp-left-14-600">{filterBy?.checkOut ? new Date(filterBy.checkOut).toLocaleDateString(): <span className="grey-71">Add date</span>}</div>
-              </div>
-              
-            </button>
-            
-              {isDateClicked && <div className="day-picker-modal"> 
-              <CalendarMain filterBy={filterBy} onChangeDate={onChangeDate} />
-              </div>}
-            <button className="order-form-btn down flex space-between center" onClick={()=>setIsGuestsClicked(!isGuestsClicked)}>
-              <div className="date-container flex column guests">
-                <div className="upp-left-14-600 bold fs9">GUESTS</div>
+          <div className="set-dates">
+            <div className="order-details-setting mar-b16">
+              <button className="order-form-btn up" onClick={toggleDatePicker}>
+                <div className="date-container flex column">
+                  <div className="upp-left-14-600 bold fs9">CHECK-IN</div>
+                  <div className="upp-left-14-600 mar-r8">
+                    {filterBy?.checkIn ? (
+                      new Date(filterBy.checkIn).toLocaleDateString()
+                    ) : (
+                      <span className="grey-71">Add date</span>
+                    )}
+                  </div>
+                </div>
+                <div className="date-container flex column">
+                  <div className="upp-left-14-600 bold fs9">CHECKOUT</div>
+                  <div className="upp-left-14-600">
+                    {filterBy?.checkOut ? (
+                      new Date(filterBy.checkOut).toLocaleDateString()
+                    ) : (
+                      <span className="grey-71">Add date</span>
+                    )}
+                  </div>
+                </div>
+              </button>
+
+              {isDateClicked && (
+                <div className="day-picker-modal">
+                  <CalendarMain
+                    filterBy={filterBy}
+                    onChangeDate={onChangeDate}
+                  />
+                </div>
+              )}
+              <button
+                className="order-form-btn down flex space-between center"
+                onClick={() => setIsGuestsClicked(!isGuestsClicked)}
+              >
+                <div className="date-container flex column guests">
+                  <div className="upp-left-14-600 bold fs9">GUESTS</div>
                   <div className="upp-left-14-600 fs14 ">
-                    {(filterBy?.adults>1 || filterBy?.children>0)? filterBy.adults+ filterBy.children + ' guests' : '1 guest'} 
-                    </div>                                       
-              </div>
-                {isGuestsClicked ? <BiChevronUp className="guests-arrow"/> : <BiChevronDown className="guests-arrow"/>}
-            </button>
-              {isGuestsClicked && <div className="guests-counter-container details-guest"><GuestsCounter filter={filterBy} onCountChange={onCountChange} parentCmp={'details'}/></div>}
+                    {filterBy?.adults > 1 || filterBy?.children > 0
+                      ? filterBy.adults + filterBy.children + " guests"
+                      : "1 guest"}
+                  </div>
+                </div>
+                {isGuestsClicked ? (
+                  <BiChevronUp className="guests-arrow" />
+                ) : (
+                  <BiChevronDown className="guests-arrow" />
+                )}
+              </button>
+              {isGuestsClicked && (
+                <div className="guests-counter-container details-guest">
+                  <GuestsCounter
+                    filter={filterBy}
+                    onCountChange={onCountChange}
+                    parentCmp={"details"}
+                  />
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-        <button
-          className="reserve-btn mar-b16"
-          onClick={() => {
-            onReserve()
-          }}
-        >
-          Reserve
-        </button>
-        <div className="order-form-msg mar-b24">You won't be charged yet</div>
-        {/* {!daysCount && 
+          <button
+            className="reserve-btn mar-b16"
+            onClick={() => {
+              onReserve();
+            }}
+          >
+            Reserve
+          </button>
+          <div className="order-form-msg mar-b24">You won't be charged yet</div>
+          {/* {!daysCount && 
         <>
         </>} */}
-        {/* {daysCount > 0 && 
+          {/* {daysCount > 0 && 
         <> */}
-        <div className="order-form-pricing mar-b24">
-          <div className="under-line">${filterBy.checkOut ?  `${utilService.toActualPrice(stay.price)} X ${calculateDays()}` : 0} </div>
-          <div>${filterBy.checkOut ? utilService.toActualPrice((stay.price) * (new Date(filterBy.checkOut)-new Date(filterBy.checkIn))/(1000 * 60 * 60 * 24)) : 0}</div>
-        </div>
-        <div className="order-form-pricing mar-b24">
-          <div className="under-line">Service fee</div>
-          <div>${serviceFee}</div>
-        </div>
-        <div className="order-form-total-price">
-          <div className="bold">Total</div>
-          <div>${filterBy.checkOut ? utilService.toActualPrice(stay.price * (new Date(filterBy.checkOut)-new Date(filterBy.checkIn))/(1000 * 60 * 60 * 24) + serviceFee) : 0}</div>
-        </div>
-        {/* </>}  */}
-        <div>
-        </div>
+          <div className="order-form-pricing mar-b24">
+            <div className="under-line">
+              $
+              {filterBy.checkOut
+                ? `${utilService.toActualPrice(
+                    stay.price
+                  )} X ${calculateDays()}`
+                : 0}{" "}
+            </div>
+            <div>
+              $
+              {filterBy.checkOut
+                ? utilService.toActualPrice(
+                    (stay.price *
+                      (new Date(filterBy.checkOut) -
+                        new Date(filterBy.checkIn))) /
+                      (1000 * 60 * 60 * 24)
+                  )
+                : 0}
+            </div>
+          </div>
+          <div className="order-form-pricing mar-b24">
+            <div className="under-line">Service fee</div>
+            <div>${serviceFee}</div>
+          </div>
+          <div className="order-form-total-price">
+            <div className="bold">Total</div>
+            <div>
+              $
+              {filterBy.checkOut
+                ? utilService.toActualPrice(
+                    (stay.price *
+                      (new Date(filterBy.checkOut) -
+                        new Date(filterBy.checkIn))) /
+                      (1000 * 60 * 60 * 24) +
+                      serviceFee
+                  )
+                : 0}
+            </div>
+          </div>
+          {/* </>}  */}
+          <div></div>
+        </section>
+        {reviewModal ? (
+          <ReviewModal closeReviewModal={closeReviewModal} stay={stay} />
+        ) : (
+          ""
+        )}
       </section>
-      </section>
-    )
+    );
 }
