6eda8ae2d4fca82e07bd3f218a9a0865246529b7
Alon Mlievski <alonmlievski@gmail.com>
2024-01-24 12:07:22 +0200

    map app header on desktop view

--- frontend/src/cmps/app-header.jsx
+++ frontend/src/cmps/app-header.jsx
@@ -15,10 +15,10 @@ import { useState } from 'react'
 import { filterService } from '../services/filterService'
 import { LoginMenu } from './login-menu'
 
-export function AppHeader({onToSearch, stay ,onClickOutSideTheBar, origin}) {
+export function AppHeader({ onToSearch, stay, onClickOutSideTheBar, origin }) {
     const openSearchBar = useSelector(storeState => storeState.stayModule.searchModalOpen)
     const navigate = useNavigate()
-    const [isMenuOpen , setIsMenuOpen] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     function onChangeStaySearchBar(option) {
         store.dispatch({
@@ -27,83 +27,98 @@ export function AppHeader({onToSearch, stay ,onClickOutSideTheBar, origin}) {
         })
     }
 
+    const navData = [
+        {
+            navKey: 'anywhere',
+            navType: 'location',
+            navName: 'anywhere'
+        },
+        {
+            navKey: 'anyWeek',
+            navType: 'checkIn',
+            navName: 'any week'
+        },
+        {
+            navKey: 'addGuests',
+            navType: 'guests',
+            navName: 'add guests'
+        },
+
+    ]
+
     return (<>
-            <main className='main-header'>
-                <div className="logo" onClick={() => navigate('/')}>
-                    <div><span className="a-Logo"><SiAirbnb /></span><span className='text-next-to-logo'>nyplace</span></div>
-                </div>
-                {origin!=='dashboard' && <div className={`center-grid ${!openSearchBar? "" : "close"}`}>
-                    <div className={!openSearchBar ? 'rounded-full nav-bar-header' : " rounded-full nav-bar-header close"}>
-                        <nav className='header-nav long'>
-                            <NavLink key={"anywhere"} onClick={() => onChangeStaySearchBar('location')}>
-                                anywhere
+        <main className='main-header'>
+            <div className="logo" onClick={() => navigate('/')}>
+                <div><span className="a-Logo"><SiAirbnb /></span><span className='text-next-to-logo'>nyplace</span></div>
+            </div>
+            {origin !== 'dashboard' && <div className={`center-grid ${!openSearchBar ? "" : "close"}`}>
+                <div className={!openSearchBar ? 'rounded-full nav-bar-header' : " rounded-full nav-bar-header close"}>
+                    <nav className='header-nav long'>
+                        {navData.map((nav) => {
+                            return (
+                                <NavLink key={`${nav.navKey}`} onClick={() => onChangeStaySearchBar(`${nav.navType}`)}>
+                                    {nav.navName}
+                                </NavLink>
+                            )
+                        })}
+                    </nav>
+                    <div className="title-to-details-page" onClick={() => onChangeStaySearchBar('location')}>
+                        Start your search
+                    </div>
+                    <div className='search-icon'>
+                        <FiSearch />
+                    </div>
+                    <nav className='header-nav short'>
+                        <div className="top">
+                            <NavLink key={"anywhere"} className="bold" onClick={() => onChangeStaySearchBar('location')}>
+                                Anywhere
                             </NavLink>
+                        </div>
+                        <div className="bottom">
                             <NavLink key={"anyWeek"} onClick={() => onChangeStaySearchBar('checkIn')}>
-                                any week
+                                Any week
                             </NavLink>
                             <NavLink key={"addGuests"} onClick={() => onChangeStaySearchBar('guests')}>
-                                add guests
+                                Add guests
                             </NavLink>
-                        </nav>
-                        <div className="title-to-details-page" onClick={() => onChangeStaySearchBar('location')}>
-                            Start your search
                         </div>
-
-
-                        <div className='search-icon'>
-                            <FiSearch />
-                        </div>
-                        <nav className='header-nav short'>
-                            <div className="top">
-                                <NavLink key={"anywhere"} className="bold" onClick={() => onChangeStaySearchBar('location')}>
-                                    Anywhere
-                                </NavLink>
-                            </div>
-                            <div className="bottom">
-                                <NavLink key={"anyWeek"} onClick={() => onChangeStaySearchBar('checkIn')}>
-                                    Any week
-                                </NavLink>
-                                <NavLink key={"addGuests"} onClick={() => onChangeStaySearchBar('guests')}>
-                                    Add guests
-                                </NavLink>
-                            </div>
-                        </nav>
-                        <div className="filter-btn">
-                            <SiAirbnb />
-                        </div>
+                    </nav>
+                    <div className="filter-btn">
+                        <SiAirbnb />
                     </div>
-                </div>}
-                <div className="users-option fs-14">
-                    <div className="header-options-switching-keys">
-                        <div className="switch-to-host">
-                            <div className='fs14 lh18 pad-all12 rounded-full bold switch-to-host'>
-                                switch to hosting</div>
-                        </div>
-                        <div className="language-option fs14 pad-all12">
-                            <GrLanguage />
-                        </div>
-                    </div>
-
-                    <div onClick={()=>setIsMenuOpen(!isMenuOpen)} className="login-menu rounded-full">
-                        <span className='menu fs-14 lh-20'>
-                            <AiOutlineMenu />
-                        </span>
-                        <span className='person mar-l12 fs12'>
-                            <IoPersonCircleSharp />
-                        </span>
-                        
-                    </div>
-                        
                 </div>
-            </main>
-            {isMenuOpen && <LoginMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>}
-            <div className='cover-filter full'>
-                <div className={openSearchBar? "open": ''}></div>
+            </div>}
+            <div className="users-option fs-14">
+                <div className="header-options-switching-keys">
+                    <div className="switch-to-host">
+                        <div className='fs14 lh18 pad-all12 rounded-full bold switch-to-host'>
+                            switch to hosting</div>
+                    </div>
+                    <div className="language-option fs14 pad-all12">
+                        <GrLanguage />
+                    </div>
+                </div>
+
+                <div onClick={() => setIsMenuOpen(!isMenuOpen)} className="login-menu rounded-full">
+                    <span className='menu fs-14 lh-20'>
+                        <AiOutlineMenu />
+                    </span>
+                    <span className='person mar-l12 fs12'>
+                        <IoPersonCircleSharp />
+                    </span>
+
+                </div>
+
             </div>
-           {!openSearchBar && <div className="line full"></div>}
-            <SearchBar onToSearch={onToSearch} onClickOutSideTheBar={onClickOutSideTheBar} />
-            {(!stay && origin!=='dashboard') && <SecondaryFilter onToSearch={onToSearch} />}
-            
-        </>
+        </main>
+        {isMenuOpen && <LoginMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />}
+        <div className='cover-filter full'>
+            <div className={openSearchBar ? "open" : ''}></div>
+        </div>
+        {!openSearchBar && <div className="line full"></div>}
+        <SearchBar onToSearch={onToSearch} onClickOutSideTheBar={onClickOutSideTheBar} />
+        {(!stay && origin !== 'dashboard') && <SecondaryFilter onToSearch={onToSearch} />}
+
+    </>
     )
 }
