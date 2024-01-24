4b9791b428e4dfb368822bcbebcbb1ee77ea2909
Alon Mlievski <alonmlievski@gmail.com>
2024-01-24 23:09:34 +0200

    adding suspense

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Suspense} from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { store } from './store/store'
 import { RootCmp } from './root-cmp'
-import './assets/styles/main.scss'  
+import {Loading} from './cmps/loading.jsx'
 
+import './assets/styles/main.scss'  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <Router>
-      <RootCmp />
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <RootCmp />
+      </Router>
+    </Suspense>
   </Provider>
 )
 
